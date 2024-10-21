400cd07f8a86f18c32392cec509d91126d97ef28
DiplineRU <artgul52@gmail.com>
2024-10-21 15:14:36 +0300

    complete work

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Map<String, Employee> employees = new HashMap<>();
+        employees.put("Gulyan", new Employee("Artem", "Gulyan"));
+
+        EmployeeBook employeeBook = new EmployeeBook(employees);
+        employeeBook.add("Pavel", "Komarov");
+        employeeBook.add("Alex", "Novikov");
+
+        System.out.println(employeeBook.size());
+        employeeBook.remove("Alex", "Novikov");
+        System.out.println(employeeBook.size());
+        employeeBook.find("Pavel", "Komarov");
     }
 }
